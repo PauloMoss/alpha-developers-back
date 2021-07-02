13a56b2f953c645faa9369f1ad8785e9cc988c4f
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-07-02 17:13:34 -0300

    Correcao para deploy

--- src/database.js
+++ src/database.js
@@ -3,17 +3,13 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const poolConfig = {
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	port: process.env.DB_PORT,
-	database: process.env.NODE_ENV === "test" ? process.env.DB_DATABASE_TEST : process.env.DB_DATABASE,
-    // connectionString: process.env.DATABASE_URL,
-    // ssl: {
-    //     rejectUnauthorized: false
-    // }
+     connectionString: process.env.DATABASE_URL,
+     ssl: {
+         rejectUnauthorized: false
+     }
 }
 
 const connection = new Pool(poolConfig);
 
 export default connection;
+
